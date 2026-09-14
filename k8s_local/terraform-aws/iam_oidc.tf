00cd0be9c1044665a4eedec6cce8e9894abe2557
AileenQuang <37393971+PhuocQuang76@users.noreply.github.com>
2026-09-13 19:28:08 -0700

    fix OIDC

--- k8s_local/terraform-aws/iam_oidc.tf
+++ k8s_local/terraform-aws/iam_oidc.tf
@@ -23,10 +23,8 @@ resource "aws_iam_role" "github_actions_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            # Updated with your exact repository path
-            "token.actions.githubusercontent.com:sub" = "repo:PhuocQuang76/TransferFile_S3_Azure:*"
+            # Exact GitHub repo + branch that is allowed to assume this role
+            "token.actions.githubusercontent.com:sub" = "repo:PhuocQuang76/TransferFile_S3_Azure:ref:refs/heads/main"
           }
         }
       }
